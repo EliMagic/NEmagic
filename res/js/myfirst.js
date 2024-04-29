ce10f2980232a03f852e6a372b68dfa9861deadc
EliMagic <168456969+EliMagic@users.noreply.github.com>
2024-04-29 12:16:06 -0500

    Delete res/js/myfirst.js

--- res/js/myfirst.js
+++
@@ -1,8 +0,0 @@
-// https://www.w3schools.com/jsref/met_document_createelement.asp
-const activityMessage = document.createElement("p");
-activityMessage.innerText = "This is my first external JavaScript test!";
-document.body.appendChild(activityMessage);
-
-const paragraph = document.getElementsByTagName("p");
-paragraph[0].style.textAlign = "center";
-
