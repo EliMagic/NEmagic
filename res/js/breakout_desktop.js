570c41e61e5a1d32f528e8cc91d5ac0cac3f6a78
EliMagic <168456969+EliMagic@users.noreply.github.com>
2024-05-16 03:07:22 -0500

    Update breakout_desktop.js

--- res/js/breakout_desktop.js
+++ res/js/breakout_desktop.js
@@ -57,8 +57,8 @@ function adjustCanvasSize() {
     let canvasWidth, canvasHeight;
     if (isMobileDevice()) {
         // For mobile devices
-        canvasWidth = screenWidth - (2 * (margin - (margin * 0.10)) * screenWidth);
-        canvasHeight = screenHeight - (2 * (margin - (margin * 0.05)) * screenHeight);
+        canvasWidth = screenWidth - (2 * (margin - (margin * 0.20)) * screenWidth);
+        canvasHeight = screenHeight - (2 * (margin - (margin * 0.20)) * screenHeight);
     } else {
         // For desktop devices
         canvasWidth = 1280; // Your default canvas width for desktop
