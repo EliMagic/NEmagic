ddb2659d1bd94d193eb9f7637b305e2b3f2429dd
EliMagic <168456969+EliMagic@users.noreply.github.com>
2024-05-16 03:34:05 -0500

    Update breakout_desktop.js

--- res/js/breakout_desktop.js
+++ res/js/breakout_desktop.js
@@ -48,9 +48,11 @@ function isMobileDevice() {
 }
 
 function adjustCanvasSize() {
+
+    // const margin = 0.05; // 5% margin
+    const margin_width = 0.05; // 5% margin  
+    const margin_height = 0.05; // 5% margin
     
-    const margin_height = 0.9; // 5% margin
-    const margin_width = 0.8; // 5% margin
     const screenWidth = window.innerWidth;
     const screenHeight = window.innerHeight;
 
@@ -58,8 +60,10 @@ function adjustCanvasSize() {
     let canvasWidth, canvasHeight;
     if (isMobileDevice()) {
         // For mobile devices
-        canvasWidth = screenWidth * margin_width;
-        canvasHeight = screenHeight * margin_heigh;
+        // canvasWidth = screenWidth - (2 * margin * screenWidth); // 972
+        // canvasHeight = screenHeight - (2 * margin * screenHeight); // 1728
+        canvasWidth = screenWidth - ((2 * margin_width * screenWidth) * 1.5); // 972
+        canvasHeight = screenHeight - ((2 * margin_height * screenHeight) * 1.5); // 1728
     } else {
         // For desktop devices
         canvasWidth = 1280; // Your default canvas width for desktop
